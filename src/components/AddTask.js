28d93a4ee3bf1404782f0b9025369a16c558ddcf
Johnny <johnnyyvard@me.com>
2020-12-04 15:16:23 +0100

    Réalisation des redirections avec react-router

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -5,6 +5,7 @@ class AddTask extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault()
         this.props.onAddTask(this.newTask.value)
+        this.props.history.push('/')
     }
 
     render() {
