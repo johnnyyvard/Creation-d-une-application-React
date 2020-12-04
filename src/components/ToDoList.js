fb130d15aa25df534c6ddd7482c2110408ffa6c2
Johnny <johnnyyvard@me.com>
2020-12-04 12:05:45 +0100

    Affichage des données dynamiques

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,25 +1,13 @@
 import React from 'react'
+import ToDo from './ToDo'
 
-const ToDoList = () => (
+const ToDoList = ({ tasks }) => (
     <>
         <h1 className="m-3">Liste de tâches</h1>
         <ul className="list-group m-3">
-            <li className="list-group-item d-flex align-tiems-center">
-                Ranger la vaisselle
-  <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-            </li>
-            <li className="list-group-item d-flex align-tiems-center">
-                Répondre appel d'offres
-  <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-            </li>
-            <li className="list-group-item d-flex align-tiems-center">
-                Signer contrat
-  <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-            </li>
-            <li className="list-group-item d-flex align-tiems-center">
-                Ranger la salon
-  <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-            </li>
+            {
+                tasks.map((task) => <ToDo task={task} key={task.id} />)
+            }
         </ul>
     </>
 
