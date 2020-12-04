e06749f53d2947790c39eea2a56d9d58583fc085
Johnny <johnnyyvard@me.com>
2020-12-04 11:47:55 +0100

    Finalisation de l'apparence de l'application

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
+import './css/ToDo.css';
 
 ReactDOM.render(<App />, document.getElementById('root'))
